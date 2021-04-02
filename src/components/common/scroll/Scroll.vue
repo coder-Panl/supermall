932030b1a77456c7acff17344099e2db2511d23b
Panl <panl@teleware.cn>
2021-04-03 00:36:24 +0800

    '完成了上拉加载更多'

--- src/components/common/scroll/Scroll.vue
+++ src/components/common/scroll/Scroll.vue
@@ -15,20 +15,48 @@
 
   export default {
     name: "Scroll",
+    props: {
+      probeType: {
+        type: Number,
+        default: 0
+      },
+      pullUpLoad: {
+        type: Boolean,
+        default: false
+      }
+    },
     data() {
       return {
         scroll: null
       }
     },
     mounted() {
+      // 1. 创建BSscroll对象
       this.scroll = new BScroll(this.$refs.wrapper, {
         observeDOM: true, // 最新的插件里面，这个不加页面不能滚动。。
-        click: true
+        click: true,
+        probeType: this.probeType,
+        pullUpLoad: this.pullUpLoad
+      })
+
+      // 2. 监听滚动的位置
+      this.scroll.on('scroll', (position) => {
+        this.$emit('scroll', position)
+      })
+
+      // 3. 监听上拉加载更多
+      this.scroll.on('pullingUp', () => {
+        this.$emit('pullingUp')
       })
     },
     methods: {
-      scrollTo(x, y, time=300) {
+      scrollTo(x, y, time=300) { // 定义返回最上面的按钮的方法，time=300相当于设置的默认值。
         this.scroll.scrollTo(x, y, time);
+      },
+      finishPullUp() {// 等数据请求完成后，并且将新的数据展示出来后，需要执行下 finishPullUp 来告诉已经加载完成了，可以再次进行上拉加载更多了。这个做个延时是避免频繁请求加载
+        setTimeout(() => {
+          this.scroll.finishPullUp()
+        }, 2000)
       }
     }
   }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,7 +2,12 @@
   <div id="home" class="wrapper">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
 
-    <scroll class="content" ref="scroll">
+    <scroll class="content"
+            ref="scroll"
+            :probe-type="3"
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
       <home-swiper :banners="banners" />
       <recommend-view :recommends="recommends"/>
       <feature-view />
@@ -10,7 +15,7 @@
       <goods-list :goods="showGoods" />
     </scroll>
 
-    <back-top @click.native="backClick"/> <!-- 如果需要监听组件的点击，需要 @click.native-->
+    <back-top @click.native="backClick" v-show="isShowBackTop"/> <!-- 如果需要监听组件的点击，需要 @click.native-->
   </div>
 </template>
 
@@ -48,7 +53,8 @@
           'new': {page: 0, list: []},
           'sell': {page: 0, list: []}
         },
-        currentType: 'pop'
+        currentType: 'pop',
+        isShowBackTop: false
       }
     },
     computed: {
@@ -82,8 +88,17 @@
         }
       },
 
+      // 回到最上面的按钮方法
       backClick() {
         this.$refs.scroll.scrollTo(0, 0, 1000); /*通过ref拿到scroll对象，然后访问scroll对象的scrollTo方法。传入 x y 和返回的时间*/
+      },
+
+      contentScroll(position) {
+          this.isShowBackTop = -position.y > 500  // 判断当滚动大于500的时候就显示出来。
+      },
+
+      loadMore() {
+        this.getHomeGoods(this.currentType) // 因为请求的方法已经写过了，所以直接调用，并且我们的这个currentType正好是记录的是当前显示的内容，所以直接传入，这个方法就会只加载当前的数据
       },
       /**
        * 网络请求的相关方法
@@ -96,10 +111,12 @@
       },
 
       getHomeGoods(type) {
-        const page = this.goods[type].page + 1
+        const page = this.goods[type].page + 1 // page的默认值初始是0.然后在created的时候，请求这个方法会加1，相当于初始化了所有的第一页数据的。然后再进行上拉加载更多的时候，会在加1 请求第二页数据
         getHomeGoods(type, page).then(res => {
           this.goods[type].list.push(...res.data.list) // ... 是可变数组解析，他会将一组数组一个个解析出来，然后挨个push进去
-          this.goods[type].page += 1;
+          this.goods[type].page += 1
+
+          this.$refs.scroll.finishPullUp() // 调用子组件的方法，来告诉页面已经上拉加载完成，可以再次进行上拉加载
         })
       }
     }
